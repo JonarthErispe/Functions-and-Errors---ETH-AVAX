405a7346ee41e7cc5b21d7de80e051aaa8e30fe3
JonarthErispe <138769535+JonarthErispe@users.noreply.github.com>
2024-06-12 01:35:41 +0800

    Add files via upload

---
+++ module1.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ErrorHandling {
+   uint256 public value;    
+    function setValue(uint256 _value) public {
+        require(_value > 100, "Value must be greater than 100.");
+        value = _value;
+    }
+
+    function doubleValue() public {
+        uint256 oldValue = value;
+        value = value * 2;
+        assert(value == oldValue * 2);
+    }
+    
+    function resetValue() public {
+        if (value == 100) {
+            revert("Value is already 100.");
+        }
+        value = 0;
+    }
+}
